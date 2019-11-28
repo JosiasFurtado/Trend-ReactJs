744173166d872d6ad8a8738f30e8803208240581
Josias Furtado <jo.junior94@gmail.com>
2019-11-28 01:28:51 -0300

    initial commit

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import Feed from './pages/Feed';
+import Profile from './pages/Profile';
 
 export default function Routes() {
     return (
