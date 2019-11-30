dbd6fb483ade84fd330c07ab104338e6aa4b2161
Josias Furtado <jo.junior94@gmail.com>
2019-11-30 02:37:52 -0300

    insert header layout

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import { IoMdNotifications, IoMdArrowDropdown } from 'react-icons/io';
 
-import { Container } from './styles';
+import { Container, Search } from './styles';
+import logo from '../../assets/logo.png';
+import imgteste from '../../assets/imgteste.jpg';
 
 export default function Header() {
-    return <Container>Header</Container>;
+    return (
+        <Container>
+            <img src={logo} alt="Trend Logo" />
+            <Search placeholder="Search" type="text" />
+            <div>
+                <IoMdNotifications size={30} />
+                <img src={imgteste} alt="avatar" />
+                <IoMdArrowDropdown size={30} />
+            </div>
+        </Container>
+    );
 }
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,8 +1,59 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-    background-color: #333;
+    background-color: #222;
     color: #fff;
     height: 55px;
     width: 100%;
+    display: flex;
+    align-content: center;
+    justify-content: space-between;
+    padding: 5px;
+    border-radius: 10px;
+
+    > img {
+        height: 40px;
+        margin-left: 15px;
+        &:hover {
+            cursor: pointer;
+        }
+    }
+
+    div {
+        display: flex;
+        align-items: center;
+        margin-left: 5px;
+
+        svg {
+            color: #fff;
+
+            &:hover {
+                cursor: pointer;
+                color: #e3e3e3;
+            }
+        }
+
+        img {
+            height: 40px;
+            border-radius: 50%;
+            border: 2px solid #fff;
+            margin-left: 15px;
+        }
+    }
+`;
+
+export const Search = styled.input`
+    height: 43px;
+    margin-left: 20px;
+    max-width: 400px;
+    width: 100%;
+    border-radius: 10px;
+
+    color: #fff;
+    padding: 5px;
+    border-color: #555;
+
+    ::placeholder {
+        color: #fefefe;
+    }
 `;
